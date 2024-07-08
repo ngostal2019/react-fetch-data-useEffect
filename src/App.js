ffcdadaf5f14ef850835a44f5ee34b26593fa65d
Staline Ngoma <ngomastaline@gmail.com>
2024-07-08 15:56:00 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [user, setUser] = useState([])
+
+  const fetchData = () => {
+    fetch("https://randomuser.me/api/?results=1")
+      .then(response => response.json())
+      .then(data => setUser(data));
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  console.log(user.results)
+  return Object.keys(user).length > 0 ? (
+    <div className="App-header">
+      <h1>Data returned</h1>
+      <h2>First name: {user.results[0].name.first}</h2>
+      <h2>Last name: {user.results[0].name.last}</h2>
+      <p>{user.results[0].name.first} is a {user.results[0].gender}</p>
+      <p>As of today, {user.results[0].name.first} is {user.results[0].dob.age}</p>
+      <p>{user.results[0].name.first} is from {user.results[0].location.country}</p>
+      <img
+        alt={user.info.seed}
+        src={user.results[0].picture.large}
+      />
+      <small>Username: {user.results[0].login.username}</small>
     </div>
+  ) : (
+    <h1>Data pending...</h1>
   );
 }
 
